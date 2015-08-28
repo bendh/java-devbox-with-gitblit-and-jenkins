7c08d3863ced8cefe4e3aa0ed4e0e66ffd1d88f3
Ben Ooms <benoomsdh@gmail.com>
2015-08-28 11:56:35 +0200

    Modified url of gitblit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Ubuntu server 14.04 based box with jenkins and gitblit installed on a tomcat ser
 
 This is a vanila installation after a vagrant up the urls are:
 
-http://localhost:8080/gitblit-1.6.2/
+http://localhost:8080/gitblit/
 http://localhost:8080/jenkins/
 
 We are using tomcat7 and openjdk7, In /etc/environment two system properties have been defined to ease life ;<)
--- provisioning.sh
+++ provisioning.sh
@@ -5,9 +5,8 @@ sudo echo 'JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/' > /etc/environment
 sudo echo 'TOMCAT_AUTODEPLOY_HOME=/var/lib/tomcat7/webapps' > /etc/environment  
 source /etc/environment
 sudo apt-get install tomcat7 tomcat7-admin -y
-wget http://dl.bintray.com/gitblit/releases/gitblit-1.6.2.war && mv  gitblit-1.6.2.war $TOMCAT_AUTODEPLOY_HOME
+wget http://dl.bintray.com/gitblit/releases/gitblit-1.6.2.war && mv  gitblit-1.6.2.war $TOMCAT_AUTODEPLOY_HOME/gitblit.war
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war && mv jenkins.war $TOMCAT_AUTODEPLOY_HOME
 sudo mkdir /usr/share/tomcat7/.jenkins
 sudo chmod a+rwx /usr/share/tomcat7/.jenkins
-# sudo service tomcat7 restart
 
